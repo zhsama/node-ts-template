52736cc56f660a9d1b7f85dd0ddf628e9c45bf65
zhsama <a602693793@gmail.com>
2024-06-24 17:22:11 +0800

    feat: add vitest config

---
+++ vitest.workspace.ts
@@ -0,0 +1,25 @@
+import { Console } from 'console'
+import { defineWorkspace } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// Disable color in test
+process.env.NO_COLOR = '1'
+process.env.FORCE_COLOR = '0'
+
+// mock Console
+global.console.Console = Console
+
+export default defineWorkspace([
+  {
+    plugins: [tsconfigPaths()],
+    test: {
+      name: 'node',
+      globals: true,
+      environment: 'node',
+      testTimeout: 30000,
+      restoreMocks: true,
+      include: ['src/**/*.test.ts'],
+      exclude: ['**/node_modules/**'],
+    },
+  },
+])
